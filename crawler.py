2dc20c63d0b676b2bd32ea9c8a4b6a4c3e20abc0
DesolateElf-dev <54561697+DesolateElf-dev@users.noreply.github.com>
2020-05-31 21:24:57 -0300

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -3,6 +3,78 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import re
 import nltk
+import pymysql
+
+def inserePalavraLocalizacao(idurl, idpalavra, localizacao):
+    conexao = pymysql.connect(host='localhost', user='root',passwd='351246Ed*', db='indice', autocommit = True)
+    cursor = conexao.cursor()
+    cursor.execute('insert into palavra_localizacao (idurl, idpalavra, localizacao) values (%s, %s, %s)', (idurl, idpalavra, localizacao))
+    idpalavra_localizacao = cursor.lastrowid
+    cursor.close()
+    conexao.close()
+    return idpalavra_localizacao
+
+
+def inserePalavra(palavra):
+    conexao = pymysql.connect(host='localhost', user='root',passwd='351246Ed*', db='indice', autocommit = True, use_unicode = True, charset = 'utf8mb4')
+    cursor = conexao.cursor()
+    cursor.execute('insert into palavras (palavra) values (%s)', palavra)
+    idpalavra = cursor.lastrowid 
+    cursor.close()
+    conexao.close()
+    return idpalavra
+    
+
+def palavraIndexada(palavra):
+    retorno = -1 #não existe a palavra no indice
+    conexao = pymysql.connect(host='localhost', user='root',passwd='351246Ed*', db='indice', use_unicode = True, charset = 'utf8mb4')
+    cursor = conexao.cursor()
+    cursor.execute('select idpalavra from palavras where palavra = %s', palavra)
+    if cursor.rowcount > 0:
+        #print("Palavra já cadastrada")
+        retorno = cursor.fetchone()[0]
+    #else:
+        #print("Palavra não cadastrada")
+    cursor.close()
+    conexao.close()
+    return retorno
+
+
+def inserePagina(url):
+    conexao = pymysql.connect(host='localhost', user='root',passwd='351246Ed*', db='indice', autocommit = True)
+    cursor = conexao.cursor()
+    cursor.execute('insert into urls (url) values (%s)', url)
+    idpagina = cursor.lastrowid
+    cursor.close()
+    conexao.close()
+    return idpagina
+
+
+def paginaIndexada(url):
+    retorno = -1 #se retornar -1 significa que não existe a página.
+    conexao = pymysql.connect(host='localhost', user='root',passwd='351246Ed*', db='indice')
+    cursorUrl = conexao.cursor()
+    cursorUrl.execute('select idurl from urls where url = %s', url)    
+    if cursorUrl.rowcount > 0:
+        #print("Url cadastrada")
+        idurl = cursorUrl.fetchone()[0]
+        cursorPalavra = conexao.cursor()
+        cursorPalavra.execute('select idurl from palavra_localizacao where idurl = %s', idurl)
+        if cursorPalavra.rowcount > 0:
+            #print("Url com palavras")
+            retorno = -2 #se retornar -2 significa que existe a página com palavras
+        else:
+            #print("Url sem palavras cadastradas")
+            retorno = idurl #existe a página sem palavras, então retorna ID da página
+        
+        cursorPalavra.close()
+    #else:
+        #print("Url não cadastrada")
+    
+    cursorUrl.close()
+    conexao.close()
+    return retorno
+
 
 def separaPalavras(texto):
     stop = nltk.corpus.stopwords.words('portuguese')
@@ -23,6 +95,26 @@ def getTexto(sopa):
     return ' '.join(sopa.stripped_strings)
 
 
+def indexador(url, sopa):
+    indexada = paginaIndexada(url)
+    if indexada == -2:
+        print("URL já indexada")
+        return
+    elif indexada == -1:
+        idnova_pagina = inserePagina(url)
+    elif indexada > 0:
+        idnova_pagina = indexada
+    print("Indexando "+ url)
+    texto = getTexto(sopa)
+    palavras = separaPalavras(texto)
+    for i in range(len(palavras)):
+        palavra = palavras[i]
+        idpalavra = palavraIndexada(palavra)
+        if idpalavra == -1:
+            idpalavra = inserePalavra(palavra)
+        inserePalavraLocalizacao(idnova_pagina, idpalavra, i)
+
+
 def crawl(paginas, profundidade):
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
     for i in range(profundidade):
@@ -35,6 +127,7 @@ def crawl(paginas, profundidade):
                 print('Erro ao abrir a página',+pagina)
                 continue
             sopa = BeautifulSoup(dados_pagina.data, "lxml")
+            indexador(pagina, sopa)
             links = sopa.find_all('a')
             contador = 1
             for link in links:
@@ -60,4 +153,4 @@ def crawl(paginas, profundidade):
 
 
 listapaginas = ['https://pt.wikipedia.org/wiki/Linguagem_de_programa%C3%A7%C3%A3o']
-crawl(listapaginas, 1)
+crawl(listapaginas, 2)
